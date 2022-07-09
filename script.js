035bc8dbec9066c1a1c8c083cc71f472efd2acc8
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2022-07-09 18:03:45 +0530

    Images changed in popup box.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function setDetails(name,title1,title2,desc,url){
     document.getElementById("judge_title1").innerText = title1;
     document.getElementById("judge_title2").innerText = title2;
     document.getElementById("judge_desc").innerText = desc;
-    // document.getElementById("judge_").innerText = name;
+    document.getElementById("judge_url").src = document.getElementById(url).src;
 }
 
 function showDetails(event) {
@@ -34,19 +34,19 @@ function showDetails(event) {
     switch (event) {
         case document.getElementById("judge1"):
             var desc = "Judge 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            setDetails("Judge 1 name", "Professor", "WUSL", desc, "test");
+            setDetails("Judge 1 name", "Professor", "WUSL", desc, "judgeImg1");
             break;
         case document.getElementById("judge2"):
             var desc = "Judge 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            setDetails("Judge 2 name", "2 - Professor", "2WUSL", desc, "test");
+            setDetails("Judge 2 name", "2 - Professor", "2WUSL", desc, "judgeImg2");
             break;
         case document.getElementById("judge3"):
             var desc = "Judge 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            setDetails("Judge 3 name", "3Professor", "3WUSL", desc, "test");
+            setDetails("Judge 3 name", "3Professor", "3WUSL", desc, "judgeImg3");
             break;
         case document.getElementById("judge4"):
             var desc = "Judge 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            setDetails("Judge 4 name", "4Professor", "4WUSL", desc, "test");
+            setDetails("Judge 4 name", "4Professor", "4WUSL", desc, "judgeImg4");
             break;
     }
 }
